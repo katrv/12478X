348c3e52c97384d045018cee5d701848014dc24e
Katrina Ravichandran <katrinaravichandran@gmail.com>
2023-06-21 19:01:48 -0700

    Correct drivetrain code

--- bonnieVersion1/src/robot-config.cpp
+++ bonnieVersion1/src/robot-config.cpp
@@ -6,11 +6,11 @@ using namespace vex;
 brain Brain;
 competition Competition;
 controller Controller1;
-motor motorLF = motor(vex::PORT20, vex::gearSetting::ratio6_1, true);
+motor motorLF = motor(vex::PORT20, vex::gearSetting::ratio6_1, false);
 motor motorRF = motor(vex::PORT1, vex::gearSetting::ratio6_1, true);
 //motor motorLM = motor(vex::PORT16, vex::gearSetting::ratio6_1, true);
 //motor motorRM = motor(vex::PORT16, vex::gearSetting::ratio6_1, true);
-motor motorLB = motor(vex::PORT11, vex::gearSetting::ratio6_1, true);
+motor motorLB = motor(vex::PORT11, vex::gearSetting::ratio6_1, false);
 motor motorRB = motor(vex::PORT2, vex::gearSetting::ratio6_1, true);
 
 /**
